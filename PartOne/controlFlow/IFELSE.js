5b4084f65fb195a5793d906982e9b8704c00230d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-08-28 18:04:40 +0530

    JS

---
+++ PartOne/controlFlow/IFELSE.js
@@ -0,0 +1,27 @@
+let a = 10
+if(a < 8){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+
+if(!true){
+    console.log("ye nahi chlega")
+}else{
+    console.log("ye chlega")
+}
+
+
+
+/*
+!if (false) console.log("run");      // not executed
+!if ("") console.log("run");         // not executed
+!if (null) console.log("run");       // not executed
+!if (undefined) console.log("run");  // not executed
+!if (NaN) console.log("run");        // not executed
+!if (0n) console.log("run");         // not executed
+*/
+
+
+
