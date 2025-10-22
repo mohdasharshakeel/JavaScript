da1fe1c892735f5b9c75d8104811b4b996360eea
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-22 20:07:27 +0530

    question

--- Part-02/script.js
+++ Part-02/script.js
@@ -4,25 +4,34 @@
 // let newH = document.getElementsByClassName("brook")
 // console.log(newH)
 
-let h1 = document.getElementById("h1")
-h1.innerHTML = "Hello World form JS"
+// let h1 = document.getElementById("h1")
+// h1.innerHTML = "Hello World form JS"
 
 // let atti = document.querySelector("a")
 // atti.setAttribute("href", "https://www.google.com")
 // atti.innerText = "Go to Google"
 
-let atti = document.querySelector("a")
-atti.getAttribute("href", "https://www.google.com")
+// let atti = document.querySelector("a")
+// atti.getAttribute("href", "https://www.google.com")
 
-let img = document.querySelector("img")
-img.setAttribute(
-  "src",
-  "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-);
+// let img = document.querySelector("img")
+// img.setAttribute(
+//   "src",
+//   "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+// );
 
-img.style.width = "100px"
+// img.style.width = "100px"
 
-let p = document.createElement("p")
-p.innerText = "This is a new paragraph"
-// document.querySelector("body").append(p)
-document.querySelector("body").prepend(p)
+// let p = document.createElement("p")
+// p.innerText = "This is a new paragraph"
+// // document.querySelector("body").append(p)
+// document.querySelector("body").prepend(p)
+
+
+let lis = document.querySelectorAll("li")
+
+lis.forEach((val) => {
+  val.style.color = "red"
+  val.style.fontSize = "20px"
+  val.style.fontFamily = "gilroy"
+})
