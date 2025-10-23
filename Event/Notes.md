3b48dbaf5fe6f0f60dfb7ac84c3bb53788d8dffe
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-23 11:34:46 +0530

    yead

--- Event/Notes.md
+++ Event/Notes.md
@@ -1,3 +1,7 @@
 # What is event 
  - event ka matlab hota hai kuch action hua 
  - event listener ka matlab hota hai maine koi action ka reaction diya jaie ki maine click kiya to screen laal ho gyi to jo click kiya wo action aur jo screen lal hui wo reaction 
+
+# remove listener
+
+- agar humko koi listerner hatana hai to  ye krna pdega ki jb wo lister add kr rhe hai to uska function bhar likhna pdega aur phir wha us function ko pss kr dena pdega same function remove me bhi pss klrna pdega     
--- Event/script.js
+++ Event/script.js
@@ -1,4 +1,16 @@
-let h1 = document.querySelector("li");
-h1.addEventListener("click", function () {
-    h1.style.color = "red";
+// let h1 = document.querySelector("li");
+// h1.addEventListener("click", function () {
+//     h1.style.color = "red";
+// })
+
+// let h2 = document.querySelector("h2");
+// h2.addEventListener('dblclick', function () {
+//     h2.style.color = "blue";
+//     h2.style.backgroundColor = "yellow";
+//     h2.style.fontFamily = "cursive";
+// })
+
+let inn = document.querySelector("input");
+inn.addEventListener('input', function (dets) {
+    console.log(dets.data)
 })
