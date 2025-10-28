bff884184c3632641c0db44e3432f77c99bea865
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-28 13:14:59 +0530

    updated

---
+++ closure.js
@@ -0,0 +1,15 @@
+function count() {
+    let privateCounter = 0;
+    return function () {
+        privateCounter++;
+        console.log(privateCounter)
+
+    }
+
+}
+
+let counter = count();
+counter();
+counter();
+counter();
+counter();
