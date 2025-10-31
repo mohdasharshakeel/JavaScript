8ffaa4beabfdde0e5947ea5bfded6d15a0530226
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-31 20:41:41 +0530

    updated

--- closure.js
+++ closure.js
@@ -12,4 +12,3 @@ let counter = count();
 counter();
 counter();
 counter();
-counter();
--- practice.js
+++ practice.js
@@ -7,6 +7,8 @@ let obj = {
 obj.sayName(); // What will be the output? Explain why  // Output will be ReferenceError: name is not defined
 
 // Explanation:
-// In the given code, the method `sayName` is trying to access a variable `name` without any reference to the object `obj`. 
+// In the given code, the method `sayName` is trying to access a variable `name` without any reference to the object `obj`.
 // Since there is no variable named `name` defined in the scope of the function or globally, it results in a ReferenceError.
 // To access the `name` property of the object `obj`, we should use `this.name` inside the method, like this:
+
+
