c27cf76a09f2ece431e3e2081a53e56342e1ea0e
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-03 15:11:15 +0530

    mock interview 1

--- JS -Interviews/FrontendOne.js
+++ JS -Interviews/FrontendOne.js
@@ -1,1 +1,46 @@
 // This is JS Interview Question 1
+// var b = 10
+// var b = 20;
+
+// console.log(abc)
+// var abc = 10
+//? Output will be undefined
+
+// console.log(deg)
+// let deg = 20
+//? Output will be ReferenceError: Cannot access 'deg' before initialization
+
+//! what is closure in javascript?
+//! what is lexical scope?
+
+// const abc = {
+//     name: "Ashar",
+//     age: 11,
+//     address : "agra"
+// }
+
+// let xyz = {
+//     ...abc, name: "khan"
+// }
+
+// const { name, ...pqr } = abc
+
+// console.log(pqr, xyz , abc)
+
+//! what is function currying in javascript?
+
+// function sum(a) {
+//     return function (b) {
+//         return function (c) {
+//             return a + b + c
+//         }
+//     }
+// }
+
+// console.log(sum(2)(3)(4)) //9
+
+// ? Map vs ForEach
+// ! class vs funcitonal components in react
+// ! what is CDN
+// ! what is babel 
+// ! what is convention and normal routing in react router
