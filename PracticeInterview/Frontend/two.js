2d9c0aa013661bbc46c89983222f1aa275ca6e43
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-06 07:47:03 +0530

    updated

---
+++ PracticeInterview/Frontend/two.js
@@ -0,0 +1,23 @@
+console.log(0.1 + 0.2 === 0.3) // false
+console.log(0.1 + 0.2)  //! 0.30000000000000004
+
+// object to String 
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+}
+
+console.log(obj.toString());
+
+//! Example of shallow copy
+//! example of reduce method in arrau
+//! Map method in array
+
+
+//? react
+//! useLayoutEffect vs useEffect
+//! usememo vs useCallback
+//! Momoization in react
+//! 
