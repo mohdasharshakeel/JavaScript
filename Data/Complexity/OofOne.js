c7595b7b9fcf9b338d06c632125278b2b06e7c25
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-06 08:50:31 +0530

    0(1)

---
+++ Data/Complexity/OofOne.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5];
+
+const getElem = (arr, target) => {
+    console.log(`the element at index ${target} is ${arr[target]}`);
+
+}
+
+getElem(num, 3);
+
+
+//! this is the exmaple of O(1) complexity this is constant time complexity because no matter how big the array is it will always take the same time to get the element at the target index.
