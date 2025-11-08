5cd60a2ce2719db826355431a7898d994c845ab3
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-08 19:21:44 +0530

    interview 02

---
+++ FrontEnd/Interview02.js
@@ -0,0 +1,68 @@
+
+// const str = "my name is ashar"
+//! Longest word
+// function longestWord() {
+//     let longg = '';
+//     let words = str.split(' ');
+//     for (const word of words) {
+//         if (word.length > longg.length) {
+//             longg = word
+//         }
+//     }
+
+//     return longg
+// }
+
+// let res = longestWord(str);
+// console.log(res)
+
+//! second Longest
+// function longestWord(str) {
+//   let longg = "";
+//   let secLongest = "";
+
+//   let words = str.split(" ");
+
+//   for (const word of words) {
+//     if (word.length > longg.length) {
+//       secLongest = longg;
+//       longg = word;
+//     } else if (word.length > secLongest.length && word.length < longg.length) {
+//       secLongest = word;
+//     }
+//   }
+
+//   return secLongest;
+// }
+
+// let res = longestWord(str);
+// console.log(res);
+//! two sum
+
+// let arr = [10, 28, 7, 6, 11];
+// let ans = 16;
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] + arr[j] == ans) {
+//       console.log(`${i} and ${j}`);
+//     }
+//   }
+// }
+
+// console.log(isNaN('ashar'))
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let ans = arr.map((item) => item = 5)
+// console.log(ans)
+
+// ? Reacts -------
+
+//! useState vs useReducer
+//! useMemo vs useCallback
+//! how to handle form validation
+//! how to optimize -  useMemo . useCallback , code spliting debounce throttling , lazy loading
+//! framework  vs  library
+//! dynamic routing
+//! class a=vs funcational components
+//! avoid pop driling 
