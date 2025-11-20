cb067c50d95a9278426e6d520386c863501deb17
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-20 15:14:59 +0530

    full added

--- FrontEnd/interview03.js
+++ FrontEnd/interview03.js
@@ -1,1 +1,53 @@
-// Interview 03 of Frontend development 
+// Interview 03 of Frontend development
+
+// let x = {},
+//     y = { name: "madara" };
+//     z = { name: "luffy" };
+
+// x[y] = { name: "zoro" };
+// x[z] = { name: "naruto" };
+
+// console.log(x[y])
+
+
+// let a = { name: "Ashar", age: 21, city: "agra" };
+// let b = { ...a, name: "Luffy" };
+// const { name, ...xyz } = a;
+// console.log(xyz, b)
+
+
+// console.log(pqr); // undefined;
+// var pqr = 100;
+
+// var x = "ashar";
+// console.log(` the name is ${x}`);
+
+
+//! First order funcation
+//! Debouce and throttling
+
+
+//? React
+//* mouting face , and unmounting face , in RS
+//* useReducer hook and useState
+//* dev vs normal dependency
+//* useMemo vs use
+//* obtimizing React Application
+//* Keys in react
+//* Map method
+//* Dynamic routing
+//* for routing purpose why Packege we install
+//* vertial DOM vs DOM
+//* Why react first
+
+
+//? HTML
+
+//* self padding and self spacing
+//* proress tag vs meter tag
+
+
+//? CSS
+
+//* inline vs block element
+//* types of selectors
