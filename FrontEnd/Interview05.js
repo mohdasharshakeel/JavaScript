9ac9ad11668e311575f7d5647ea9ed39eda77184
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-11-25 17:43:55 +0530

    commit

---
+++ FrontEnd/Interview05.js
@@ -0,0 +1,10 @@
+//! why we use jion method
+//! why we use split method
+
+
+// let str = "ashar";
+// let splittedString = str.split("").reverse().join("");
+// console.log(splittedString)
+
+// let  a ;
+// console.log(a)  //! Reference 
