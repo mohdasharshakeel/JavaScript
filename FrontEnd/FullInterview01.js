58836ed3080c11ec242814a0379491ceadf2ae74
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-12-16 11:11:43 +0530

    added

---
+++ FrontEnd/FullInterview01.js
@@ -0,0 +1,60 @@
+//! Introduction :-
+
+//! javascript :-
+//* let vs var vs const :-
+//* event loop micro task mini micro task
+//* what are the closure
+//* what is TDZ
+//* this keyword : in arrow function
+//* aysnc vs sync
+//* == vs ===
+//* pure fuction
+//* map vs filter vs reduce
+//* shallow copy vs deep copy
+//* promise.all()  vs promise.rasise()
+
+//? move zero with breaking non zero order
+// let arr = [0, 0, 1, 3, 12];
+
+// let result = [];
+// let zeroCount = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 0) {
+//     zeroCount++;
+//   } else {
+//     result.push(arr[i]);
+//   }
+// }
+
+// while (zeroCount > 0) {
+//   result.push(0);
+//   zeroCount--;
+// }
+
+// console.log(result);
+
+//? missing number
+
+//  let arr = [0, 1, 2,3,4,6];
+
+//  for (let i = 0; i <= arr.length; i++) {
+//    if (!arr.includes(i)) {
+//      console.log(i);
+//      break;
+//    }
+//  }
+
+
+//! React :-
+//* hooks
+//* virtual vs normal DOM
+//* useEffect
+//* useLayout
+//* control and unControlled
+//* what is pop drilling. and how we can avoid it
+//* why keys are important
+//* how we can optimize react [ reactMemo, lazyLoading, splitig code ]
+//* what is custome hook
+//* useRef vs useState
+//* 
