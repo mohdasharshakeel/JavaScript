a2e92562a84a0e451620f81486a2ef4a3f3e3857
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-12-16 10:38:14 +0530

    update

---
+++ FrontEnd/INterview07.js
@@ -0,0 +1,19 @@
+//! difference between == vs ===
+// console.log([] == []) //! false
+// console.log([] === []) //! false
+//! difference between every and some method
+//! then vs finally     - funcanlity differnece
+//! are arrow fun use as constructors ?
+var a = 50
+function def() {
+    var a = 5000;
+}
+
+let b = a;
+function abc() {
+    let b = 50000
+}
+
+console.log(b)
+ 
+ 
