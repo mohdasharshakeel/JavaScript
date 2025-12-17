6b6f02159eeffe2fc306356dbcbbe9e51d77c50d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-12-17 10:01:48 +0530

    interview

---
+++ FrontEnd/Interview08.js
@@ -0,0 +1,17 @@
+//! Introduction ;  So, My name is Mohd Ashar Shakeel and i am full stack Engineer
+
+//! :- Javascript
+
+//* Let Var const
+//* hoisting
+//* spread vs rest operator
+
+//? Rest operator example
+// function abc(a,...b) {
+//     console.log(`value of a is ${a} and value of b is ${b}`)
+// }
+
+// abc(1,2,3,4,5,6,7);
+
+//* Async in JS
+//* arrow function vs normal function 
