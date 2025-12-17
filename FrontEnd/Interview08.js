0f8aad18ba46c580a5aa3ee83626c07fd76a6165
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-12-17 10:15:00 +0530

    push

--- FrontEnd/Interview08.js
+++ FrontEnd/Interview08.js
@@ -14,4 +14,23 @@
 // abc(1,2,3,4,5,6,7);
 
 //* Async in JS
-//* arrow function vs normal function 
+//* arrow function vs normal function
+//* Map filter reduce
+//* closure
+
+//! React
+//* state vs props
+//* hooks in reactJS
+//* prop driling reactJS
+
+//? remove dublicate from an array 
+const data = [1, 12, 3, 3, 5, 6, 7, 8, 9, 9];
+
+const unique = data.filter((item, index) => {
+  return data.indexOf(item) === index;
+});
+
+console.log(unique);
+
+
+
