399210bceddd86080f72b3cd3c1073c95adeeb01
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-09 09:55:22 +0530

    push

--- FrontEnd/Interview09.js
+++ FrontEnd/Interview09.js
@@ -1,3 +1,3 @@
 let game = 500000000
 let gameTwo = 50000
-let str = "kl se phadai start yr pakkaaa yar pakka monday se jyeahh kokk kikikiki hnhnh jk"
+let str = "kl se phadai start yr pakkaaa yar pakka monday se jyeahh kokk kikikiki hnhnh jk oakokao"
