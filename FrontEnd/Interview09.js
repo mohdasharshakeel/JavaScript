153a426b2c16f8d4e8119b2332fd3a3580087dc1
mohdasharshakeel <mohdasharshakeel@gmail.com>
2026-01-16 09:44:27 +0530

    push

--- FrontEnd/Interview09.js
+++ FrontEnd/Interview09.js
@@ -1,4 +1,4 @@
 let game = 500000000
 let gameTwo = 50000
 let str = "kl se phadai start yr pakkaaa yar pakka monday se jyeahh kokk kikikiki hnhnh jk oakokao mjmjmggd"
-let arr = [1,2,3,4,5,6]
+let arr = [1,2,3,4,5,6,7,8]
